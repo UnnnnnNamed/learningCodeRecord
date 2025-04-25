d83d2356c32c0e21fad01616d1454fe79d101463
WOoOD <90784639+UnnnnnNamed@users.noreply.github.com>
2025-04-25 17:16:17 +0800

    Delete Calculator directory

--- Calculator/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include <QPushButton>
-#include "widget.h"
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    widget w;
-    w.show();
-    return QApplication::exec();
-}
